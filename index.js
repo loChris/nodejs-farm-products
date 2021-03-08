8bd14c962bd05588631b0a1f3d1ce632b9377e10
loChris <44450518+loChris@users.noreply.github.com>
2021-03-08 11:08:41 -0400

    can now create new products and add them to db

--- index.js
+++ index.js
@@ -18,15 +18,28 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/products', async (req, res) => {
 	const products = await Product.find({});
 	res.render('products/index', { products });
+});
+
+app.get('/products/new', (req, res) => {
+	res.render('products/new');
 });
 
 app.get('/products/:id', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findById(id);
 	res.render('products/show', { product });
+});
+
+app.post('/products', async (req, res) => {
+	const newProduct = new Product(req.body);
+	await newProduct.save();
+	console.log(newProduct);
+	res.redirect(`/products/${newProduct._id}`);
 });
 
 app.listen(PORT, () => {
