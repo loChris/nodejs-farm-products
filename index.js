5195df934cb82469325b9c2463ea69be9377f52b
loChris <44450518+loChris@users.noreply.github.com>
2021-03-15 13:38:23 -0400

    can now add products to a farm and save to db

--- index.js
+++ index.js
@@ -42,6 +42,23 @@ app.get('/farms/:id', async (req, res) => {
 	res.render('farms/show', { farm });
 });
 
+app.get('/farms/:id/products/new', (req, res) => {
+	const { id } = req.params;
+	res.render('products/new', { id, categories });
+});
+
+app.post('/farms/:id/products', async (req, res) => {
+	const { id } = req.params;
+	const farm = await Farm.findById(id);
+	const { name, price, category } = req.body;
+	const product = new Product({ name, price, category });
+	farm.products.push(product);
+	product.farm = farm;
+	await farm.save();
+	await product.save();
+	res.send(farm);
+});
+
 app.post('/farms', async (req, res) => {
 	const farm = new Farm(req.body);
 	await farm.save();
